65adf02a9c39f20a856a8a6a6bd1ccef0d871396
tenubar <solartech4756@gmail.com>
2025-01-30 23:40:28 -0400

    Updated JavaScript files with the latest changes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
 export const DB_USER = process.env.DB_USER
 export const DB_HOST = process.env.DB_HOST
 export const DB_PASSWORD = process.env.DB_PASSWORD
